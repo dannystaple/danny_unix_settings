976a5d23d3c3cef320a93fc79058112881300508
Danny Staple <dstaple@nds.com>
2011-08-04 13:40:19 +0100

    ensuring scripts are put into ~/bin, and adding the jenkins one

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #Run in the tools dir. Deploys the new settings on a target machine
+#rc files
+RC_FILES=.emacs
+
 ln -s `pwd`/.emacs ~/.emacs || true
-ln -s `pwd`/python_check_and_tests.sh ~/python_check_and_tests.sh || true
+
+#bin tools
+BIN_APPS=python_check_and_tests.sh submit_xml_as_jenkins_job.sh
+for APP in ${BIN_APPS}; do
+    ln -s `pwd`/${APP} ~/bin/${APP} || true
+done
+
