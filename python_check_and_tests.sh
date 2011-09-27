359ea1d2fb04540eb0184168b301ff53667e6934
stapled <dstaple@nds.com>
2011-09-27 13:26:39 +0100

    Adding more feedback during the run

--- python_check_and_tests.sh
+++ python_check_and_tests.sh
@@ -3,7 +3,11 @@ set -eu
 test=$1
 pushd `dirname $test`
 pylint --errors-only --output-format=parseable *.py
+echo "No pylint errors found..."
 if [[ $test =~ test.*\.py ]]; then
+    echo "Tests found.. testing..."
     python $test
+    echo "Testing succeeded..."
 fi
+echo "Warnings and lint..."
 pylint --output-format=parseable --disable=C0301,C0103 *.py
